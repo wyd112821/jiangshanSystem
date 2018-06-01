ddcf73c439fe25088016557fab4d0927d0dec19a
wuyadong <985979261@qq.com>
2018-06-01 11:05:10 +0800

    a new adjust

--- media/js/common.js
+++ media/js/common.js
@@ -1,6 +1,6 @@
 
 //点击事件
-mui('#pullrefresh, #footer-jump, #offCanvasSideScroll, #content-jump').on('tap', 'a', function() {
+mui('#pullrefresh, #footer-jump, #offCanvasSideScroll, #content-jump, .guide-box').on('tap', 'a', function() {
 	var id = this.getAttribute('href');
 	if(id == "javascirpt:void(0)" || id == ""){
 		return false;
---
+++ media/js/json.js
@@ -0,0 +1,56 @@
+var level3 = [{
+	"name": "2013",
+	"value": "1",
+	"child": [{
+		"name": "1",
+		"value": "3"
+	}, {
+		"name": "2",
+		"value": "4"
+	}, {
+		"name": "3",
+		"value": "5"
+	}, {
+		"name": "4",
+		"value": "6"
+	}, {
+		"name": "5",
+		"value": "5"
+	}, {
+		"name": "6",
+		"value": "6"
+	}, {
+		"name": "7",
+		"value": "7"
+	}, {
+		"name": "8",
+		"value": "8"
+	}, {
+		"name": "9",
+		"value": "9"
+	}, {
+		"name": "10",
+		"value": "10"
+	}, {
+		"name": "11",
+		"value": "11"
+	}, {
+		"name": "12",
+		"value": "12"
+	}]
+}, {
+	"name": "2014",
+	"value": "2"
+}, {
+	"name": "2015",
+	"value": "3"
+}, {
+	"name": "2016",
+	"value": "4"
+}, {
+	"name": "2017",
+	"value": "5"
+}, {
+	"name": "2018",
+	"value": "6"
+}];
